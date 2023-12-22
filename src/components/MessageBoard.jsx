9438e849faace70ff73391c1bcfd0c9e0e81dcb2
nattawat <frank.nattawat@hotmail.com>
2023-12-22 13:56:54 +0700

    feat:complete ex

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,4 +1,26 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [outputMessage, setOutputMessage] = useState([]);
+  const [inputMessage, setInputMessage] = useState("");
+
+  const addOutputMessage = (event) => {
+    event.preventDefault();
+    const newOutputMessage = [...outputMessage];
+    newOutputMessage.push(inputMessage);
+    setOutputMessage(newOutputMessage);
+    setInputMessage("");
+  };
+
+  const listMessage = outputMessage.map((item) => {
+    return (
+      <div className="message">
+        <h1>{item}</h1>
+        <button className="delete-button">x</button>
+      </div>
+    );
+  });
+
   return (
     <div className="app-wrapper">
       <h1 class="app-title">Message board</h1>
@@ -8,17 +30,18 @@ function MessageBoard() {
             id="message-text"
             name="message-text"
             type="text"
+            onChange={(event) => {
+              setInputMessage(event.target.value);
+            }}
+            value={inputMessage}
             placeholder="Enter message here"
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button className="submit-message-button" onClick={addOutputMessage}>
+          Submit
+        </button>
       </div>
-      <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
-      </div>
+      <div class="board">{listMessage}</div>
     </div>
   );
 }
